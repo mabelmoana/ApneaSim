c4ae5fefa7a0a078c27f9487943be9730992c4a4
mabelmoana <mabelmoana@gmail.com>
2025-08-27 09:39:00 +0200

    Create MainActivity.kt

---
+++ app/app/src/main/app/src/main/java/com/apneasim/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.apneasim
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.apneasim.ui.ParamsEditorScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ParamsEditorScreen()
+        }
+    }
+}
